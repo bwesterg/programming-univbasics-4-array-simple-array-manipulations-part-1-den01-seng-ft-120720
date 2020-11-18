338af125d75cb5787227e8a0272334d52a0a6a4c
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 21:07:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,5 @@
-artists = ["Hozier", "Ariana Grande", "Usher"]
-next_artist = "Beyonce"
-artists.push(next_artist)
+def using_push
+  artists = ["Hozier", "Ariana Grande", "Usher"]
+  next_artist = "Beyonce"
+  artists.push(next_artist)
+end
